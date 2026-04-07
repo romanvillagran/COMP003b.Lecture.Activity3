946498d9d2f9d7d07bd13c559d8e32d564eacca2
cbig3 <romanvillagran386@gmail.com>
2026-04-06 18:34:49 -0700

    Add project files.

---
+++ COMP003B.LectureActivity3/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.LectureActivity3.Models
+{
+    public class Product
+    {
+        public int ID {  get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+    }
+}
