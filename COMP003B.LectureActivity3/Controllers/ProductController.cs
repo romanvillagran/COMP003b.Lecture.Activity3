17fd2ec148f0e72674ec4941a49545e39edb149c
cbig3 <romanvillagran386@gmail.com>
2026-04-06 19:03:47 -0700

    addded the controller and everything for it

---
+++ COMP003B.LectureActivity3/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using COMP003B.LectureActivity3.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace COMP003B.LectureActivity3.Controllers
+{
+    [Route("product")]
+    public class ProductController : Controller
+    {
+        [HttpGet("create")]
+        public IActionResult Creaate()
+        {
+            return View();
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromForm]Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            return RedirectToAction("Success", product);
+        }
+
+        [Route("success")]
+
+        public IActionResult Success(Product product)
+        {
+            return View(product);
+        }
+        
+    }
+}
